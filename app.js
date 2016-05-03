d6ce1ccd5363f39f40e7fc5df9fd79c53de59478
BramHendriksDeveloper <bram5688@gmail.com>
2016-05-03 14:03:19 +0200

    empty Help page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var help = require('./routes/Help');
 var buddies = require('./routes/Buddies');
 var champions = require('./routes/Champions');
 
@@ -33,6 +34,7 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', routes);
+app.use('/Help', help);
 app.use('/Buddies', buddies);
 app.use('/Champions', champions);
 
